50177c1d7cf3d8cc2ae4f644e757a01666a166dd
Tobias Aichinger <aichinger.tobias13@gmail.com>
2025-12-08 14:28:05 +0100

    added square root and absolute value for floats
    TODO: implement block sort

--- std/math.c
+++ std/math.c
@@ -2,6 +2,19 @@
 #define MIN(a, b)   ((a) < (b) ? (a) : (b))
 #define ABS(a)      ((a) > ((s64)0) ? (a) : (-a))
 
+f64 fabs(f64 a) {
+    struct MOB_FLOAT_U64 {
+        union {
+            u64 uval;
+            f64 fval;
+        };
+    } value;
+
+    value.fval = a;
+    value.uval &= 0x8000000000000000u - 1u;
+    return value.fval;
+}
+
 u64 pow(u64 base, u16 exponent) {
     if (exponent == 0) {
         return 1;
@@ -18,4 +31,16 @@ u64 pow(u64 base, u16 exponent) {
     }
 
     return ans;
+}
+
+f64 hsqrt(u64 num) {
+    f64 fnumber = num;
+    f64 initial = fnumber / 2.0;
+    f64 epsilon = 1e-5;
+
+    while (fabs(fnumber - initial * initial) >= epsilon) {
+        initial = (initial + fnumber / initial) / 2.0;
+    }
+
+    return initial;
 }
--- std/sort.c
+++ std/sort.c
@@ -1,6 +1,36 @@
 // NOTE: merge_sort for the dynamic array implementation provided by mob
 
-#define sort (array, T, cmp) ((void*)0)
+//#define merge_sort(mob_array_to_sort, sort_ascending, mob_internal_cmp)     \
+//
+//
+//
+//void merge_sort(void *array, u64 size_of_t, bool ascending, s8 (*cmp)(void *a, void *b)) {
+//
+//}
+
+#define bubble_sort_impl(T)                                     \
+    void bubble_sort_ ## T (                                    \
+            T *mob_internal_array,                              \
+            bool ascending,                                     \
+            s8 (*cmp)(T *a, T *b)) {                            \
+        for (u64 i = 0; i  < array_len(mob_internal_array); i++) {  \
+            printf("%u/%u\n", i, array_len(mob_internal_array));    \
+            for (u64 j = i + 1; j < array_len(mob_internal_array); j++) {   \
+                s8 result = cmp(&mob_internal_array[i], &mob_internal_array[j]);    \
+                if (!ascending) {                                                   \
+                    result *= -1;                                                   \
+                }                                                                   \
+                                                                                    \
+                if (result > 0) {                                                   \
+                    u8 value[sizeof(*mob_internal_array)];                          \
+                    memcpy(value, (u8*)&mob_internal_array[i], sizeof(*mob_internal_array)); \
+                    memcpy((u8*)&mob_internal_array[i], (u8*)&mob_internal_array[j], sizeof(*mob_internal_array));    \
+                    memcpy((u8*)&mob_internal_array[j], value, sizeof(*mob_internal_array)); \
+                }   \
+            }   \
+        }   \
+    }
+
 
 // TODO: think about having a macro that implements merge sort or a generic merge sort implementation
 //#define merge_sort_impl(array_to_sort, T)
@@ -11,27 +41,12 @@
 //        merge_t(mob_internal_array, sizeof(T), len, mob_internal_cmp, sort_ascending)
 //    }
 //    
-//void merge_t(void *array, u64 size_of_t, u64 array_len, s8 (*cmp)(void *a, void *b), bool sort_ascending) {
-//    if          (array_len == 1) {
-//        return;
-//    } else if   (array_len == 2) {
-//        s8 result = cmp(array, array + size_of_t);
-//
-//        if ((result == 1 && sort_ascending) || (result == -1 && !sort_ascending)) {
-//            u8 tmp[size_of_t];
-//            memcpy(tmp, array, size_of_t);
-//            memcpy(array, array + size_of_t, size_of_t);
-//            memcpy(array + size_of_t, tmp, size_of_t);
-//        }
-//
-//        return;
-//    }
-//
-//    u64 mid = array_len / 2;
-//    merge_t(array, size_of_t, mid, 
+//void merge_t(
+//        void *array, 
+//        u64 size_of_t, u64 array_len, s8 (*cmp)(void *a, void *b), bool sort_ascending) {
 //
 //}
-
+//
 // x1 x5 x9 x2 x9 x9
 //
 // x1 x5 x9 | x2 x9 x9
